98f627ba481022aa82c6a63284fb19decb6edf62
Ragortt <113362908+Ragortt@users.noreply.github.com>
2022-09-12 16:59:16 +0300

    Add files via upload

---
+++ lab1_1.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class student {
+public:
+    string Name, Surname;
+    int Age;
+};
+
+int main()
+{
+    setlocale(0, "");
+    student stud[3];
+    cout << "Заполните информацию о студентах"<<endl;
+
+    for (int i = 0; i < 3; i++) {
+        cout<<endl << "Студент №" << i+1<<endl;
+        cout << "Имя: "; cin >> stud[i].Name;
+        cout << "Фамилия: "; cin >> stud[i].Surname;
+        cout << "Возраст: "; cin >> stud[i].Age;
+    }
+    system("pause"); system("cls");
+
+    string name, surname;
+    int age, n=0;
+    cout << "Введите параметры для поиска студента"<<endl;
+    cout << "Имя: "; cin >> name;
+    cout << "Фамилия: "; cin >> surname;
+    cout << "Возраст: "; cin >> age;
+
+    cout << endl << "Найденые совпадения" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        if (stud[i].Name == name || stud[i].Surname == surname || stud[i].Age == age) {
+            cout<< endl << "Имя: " << stud[i].Name << " Фамилия: " << stud[i].Surname << " Возраст: " << stud[i].Age;
+        }
+    }
+
+    cin.get(); cin.get();
+    return 0;
+}
